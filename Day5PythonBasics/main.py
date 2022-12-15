08048be8fae719b23e993024a68c2aff1d51f301
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-12-15 18:05:09 +0530

    5th Day Commit

---
+++ Day5PythonBasics/main.py
@@ -0,0 +1,10 @@
+'''
+Please don't remove this line
+5th Day of 100Days of code
+'''
+print("Hey I am a 'good boy'\nand the viewer also good boy/girl")
+
+print("Hello World") # don't want to print this statement
+
+print("Hey", 6, 7, sep="~", end = "009\n")
+print("Harry")
