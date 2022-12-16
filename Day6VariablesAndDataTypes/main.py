1fb80853864009060ff8808e47036c57e4c8e2e5
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-12-16 22:29:16 +0530

    6th Day Commit

---
+++ Day6VariablesAndDataTypes/main.py
@@ -0,0 +1,18 @@
+a = 1.2
+b = True
+c = "Hello"
+d = None
+
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is ", type(d))
+
+list1 = [8, 1.4, [4, 5], ["Hello", "Aniket"]]
+print(list1[0])
+
+tuple1 = ((1, "ABC"), (4, "World"))
+print(tuple1)
+
+dict1 = {"Name" : "Neeraj", "age" : 21 , "CanDrive" : False}
+print(dict1.get("Name"))
