10c42a338c27d501e719c6eb0679bd64099d01ae
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-12-17 19:23:35 +0530

    7th Day Commit

---
+++ Day7CalculatorProblem/exercise.py
@@ -0,0 +1,14 @@
+a = 5
+b = 3
+
+# Addition
+print("Addition of ", a, " and ", b, " is ", a + b)
+
+# Subtraction
+print("Subtraction of ", a, " and ", b, " is ", a - b)
+
+# Multiplication
+print("Multiplication of ", a, " and ", b, " is ", a * b)
+
+# Division
+print("Division of ", a, " and ", b, " is ", a // b)
