2370120f4db1031cfca65e2848caeec4c73bbbe7
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-12-20 12:44:48 +0530

    10th Day Commit

---
+++ Day10Input/practice.py
@@ -0,0 +1,23 @@
+a = input("Enter first number : ")
+b = input("Enter second number : ")
+
+
+
+# Addition
+print("Concatenation of", a, "and", b, "is", a + b)
+print("Addition of", a, "and", b, "is", int(a) + int(b))
+
+# Subtraction
+# print("Subtraction of", a, "and", b, "is", a - b)
+print("Subtraction of", a, "and", b, "is", int(a) - int(b))
+
+# Multiplication
+# print("Multiplication of", a, "and", b, "is", a * b)
+print("Multiplication of", a, "and", b, "is", int(a) * int(b))
+
+# Division
+# print("Division of", a, "and", b, "is", a / b)
+print("Division of", a, "and", b, "is", int(a) / int(b))
+
+# Alt + Shift + Down 
+# To replicate line
