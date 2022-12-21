c1f35df051649ebcb282ba4ba358468be4858990
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-12-21 14:55:57 +0530

    11th Day Commit

---
+++ Day11Strings/main.py
@@ -0,0 +1,24 @@
+apple = "He said, \"I want to eat an apple\" "
+apple = 'He said, "I want to eat an apple" '
+
+apple = '''Coding, sometimes called computer programming, 
+is how we communicate with computers. Code tells a computer what actions to take, 
+and writing code is like creating a set of instructions. By learning to write code, you can 
+tell computers what to do or how to behave in a much faster way. You can use 
+this skill to make websites and apps, process data, and do lots of other cool things.
+'''
+
+name = "Aniket"
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+# print(name[6]) Throws an error as string is only of length 6
+
+print("Using for loop")
+
+for ch in apple:
+    print(ch)
