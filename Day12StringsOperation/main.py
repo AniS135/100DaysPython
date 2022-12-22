8ec73877e6f3f6b3ada94a870b45e7963b434719
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-12-22 13:04:00 +0530

    11th Day Commit

---
+++ Day12StringsOperation/main.py
@@ -0,0 +1,19 @@
+names = "Aniket, Saini"
+
+print(len(names))
+
+print(names[0 : 4])
+print(names[2 : ])
+print(names[1 : 4])
+print(names[ : ])
+print(names[0 : -3])
+print(names[0 : len(names) - 3])
+print(names[-1 : ])
+print(names[len(names) - 1 : ])
+
+# names[i, j]
+# including i but not j
+
+# nm = "Harry"
+
+# print(nm[-4 : -2])
