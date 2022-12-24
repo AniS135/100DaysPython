89c24e31325af5a52aa2bafac05d6ed27f2b2e24
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-12-24 12:09:24 +0530

    14th Day Commit

---
+++ Day14IfElseCondition/if-elseCond.py
@@ -0,0 +1,7 @@
+applePrice = 210
+budget = 200
+
+if( budget - applePrice > 50):
+    print("Alexa add 1kg apple to cart")
+else:
+    print("Alexa don't add apples to cart")
---
+++ Day14IfElseCondition/main.py
@@ -0,0 +1,12 @@
+a = int(input("Enter your age : "))
+
+print("Your age is : ", a)
+
+# Condition Operator
+# >, <, >=, <=, ==, !=
+
+if(a >= 18):
+    print("You can drive")
+else:
+    print("You cannot drive")
+print("This is a seprate block from if-else")
