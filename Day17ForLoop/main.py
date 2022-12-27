99316bde4baa00dbf7ae4ed29018eaed2f5f24a4
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-12-27 12:14:51 +0530

    17th Day Commit

---
+++ Day17ForLoop/main.py
@@ -0,0 +1,18 @@
+name = "Aniket"
+
+for i in name:
+    print(i, end=" ")
+
+colors = ["Red", "Green", "Yellow", "Blue"]
+
+for color in colors:
+    print(color)
+    for i in color:
+        print(i, end=" ")
+    print()
+
+for i in range(5):
+    print(i + 1, end=" ")
+
+for i in range(0, 10, 2):
+    print(i, end=" ")
