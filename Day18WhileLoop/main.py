2bb06c64c0f8f106a5f83a0a7a870dc707d7551e
Aniket <aniket.saini.ug20@nsut.ac.in>
2022-12-28 12:49:38 +0530

    18th Day Commit

---
+++ Day18WhileLoop/main.py
@@ -0,0 +1,25 @@
+# for i in range(3):
+#     print(i)
+
+# i = int(input("Enter the number : "))
+# while(i <= 38):
+#     i = int(input("Enter the number : "))
+#     print(i)
+
+# print("Done with loop part.")
+
+# count = 5
+
+# while(count > 0):
+#     print(count)
+#     count = count - 1
+# else:
+#     print("I am inside else")
+
+while (True) :
+    i = int(input("Enter the number : "))
+    print(i)
+    if(i > 38):
+        break
+
+print("Done with while loop")
