22ba55de2d11050ff6f583f237e7ec059abb4c9b
Aniket <aniket.saini.ug20@nsut.ac.in>
2023-01-10 17:48:20 +0530

    31st Day Commit

---
+++ Day31Sets/main.py
@@ -0,0 +1,12 @@
+# Set doesn't maintain order of elements
+s = {7, 9, 2, 6}
+print(s)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+name = set()
+print(type(name))
+
+for val in info:
+    print(val)
