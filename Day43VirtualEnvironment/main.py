bfc2e83e6b58db47e113bdae5778fb92f47c9649
Aniket <aniket.saini.ug20@nsut.ac.in>
2023-01-22 12:28:38 +0530

    43rd Day Commit

---
+++ Day43VirtualEnvironment/main.py
@@ -0,0 +1,22 @@
+## Using venv we can create virtual evnvironment using python
+
+# Important command line codes
+
+# python -m venv <name> # To Create a vitual environment
+
+# To activate virtual environment
+# cmd.exe
+
+# C:\> <venv>\Scripts\activate.bat
+
+# PowerShell
+
+# PS C:\> <venv>\Scripts\Activate.ps1
+
+# To deactivate 
+# C:\> <venv>\Scripts\deactivate.bat
+
+# To create list of all packages we have installed
+# pip freeze > requirements.txt
+# And at another end it can be installed
+# pip install -r requirements.txt
