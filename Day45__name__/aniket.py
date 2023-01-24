ec2143e3d979a04614539406c3f12245268dda11
Aniket <aniket.saini.ug20@nsut.ac.in>
2023-01-24 12:30:53 +0530

    45th Day Commit

---
+++ Day45__name__/aniket.py
@@ -0,0 +1,8 @@
+
+def welcome():
+    print("Hey you are welcome from Aniket")
+
+# print(__name__)
+
+if __name__ == "__main__": # This will occur only when this file is executed directly
+    welcome()
---
+++ Day45__name__/main.py
@@ -0,0 +1,3 @@
+import aniket
+
+aniket.welcome()
