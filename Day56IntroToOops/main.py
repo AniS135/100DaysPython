91c80456434d4b1f7476eff0e3a36144f32d7cc6
Aniket <aniket.saini.ug20@nsut.ac.in>
2023-02-04 10:59:27 +0530

    56th Commit Day

---
+++ Day56IntroToOops/main.py
@@ -0,0 +1,22 @@
+def hello():
+    print("hello")
+
+hello()
+
+sales = 6000
+profit = 2000
+ad = 1000
+
+# In Programming languages, mainly there are two 
+# approaches that are used to write program or code.
+
+# 1 -> Procedural Programming
+# 2 -> Object - Oriented Programming
+
+# Example =>
+
+# RailwayForm ---> Class {blueprint}
+
+# harry -> harry ki info wala format ---> Object[entity]
+# tom -> tom ki info wala form
+# shubham -> shubham ki info wala form
