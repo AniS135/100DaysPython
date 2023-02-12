fadc191232beb12b80eaba15d5f1146a552df909
Aniket <aniket.saini.ug20@nsut.ac.in>
2023-02-12 11:44:36 +0530

    64th Day Commit

--- Day64Exercise6LibraryManagement/main.py
+++ Day64Exercise6LibraryManagement/main.py
@@ -9,15 +9,22 @@ doesnt persist the books after the program is stopped!
 
 class Library:
     def __init__(self) -> None:
-        self.num_of_books = 0
-        self.books = []
+        self.__num_of_books = 0
+        self.__books = []
     
     def add(self, book):
-        self.books.append(book)
-        self.num_of_books += 1
+        self.__books.append(book)
+        self.__num_of_books += 1
     
     def isSame(self):
-        return len(self.books) == self.num_of_books
+        return len(self.__books) == self.__num_of_books
+    
+    def printAllBooks(self):
+        print(self.__books)
+    
+    def getCount(self):
+        return self.__num_of_books
+    
 
 library1 = Library()
 library2 = Library()
@@ -32,6 +39,9 @@ library3.add("World Record")
 
 library3.add("2 States")
 
-print(library1.isSame(), library1.num_of_books)
-print(library2.isSame(), library2.num_of_books)
-print(library3.isSame(), library3.num_of_books)
+print(library1.isSame(), library1.getCount())
+library1.printAllBooks()
+print(library2.isSame(), library2.getCount())
+library2.printAllBooks()
+print(library3.isSame(), library3.getCount())
+library3.printAllBooks()
