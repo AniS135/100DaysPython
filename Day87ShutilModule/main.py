4bc34cd0eebf0840c57c32850c4e500f04ae9065
Aniket <aniket.saini.ug20@nsut.ac.in>
2023-03-07 20:58:03 +0530

    87th Day Commit

---
+++ Day87ShutilModule/main.py
@@ -0,0 +1,26 @@
+import shutil
+import os
+
+"""
+The following are some of the most commonly used functions in the shutil module:
+
+1. shutil.copy(src, dst): This function copies the file located at src to a new location specified by dst. If the destination location already exists, the original file will be overwritten.
+
+2. shutil.copy2(src, dst): This function is similar to shutil.copy, but it also preserves more metadata about the original file, such as the timestamp.
+
+3. shutil.copytree(src, dst): This function recursively copies the directory located at src to a new location specified by dst. If the destination location already exists, the original directory will be merged with it.
+
+4. shutil.move(src, dst): This function moves the file located at src to a new location specified by dst. This function is equivalent to renaming a file in most cases.
+
+5. shutil.rmtree(path): This function recursively deletes the directory located at path, along with all of its contents. This function is similar to using the rm -rf command in a shell.
+"""
+
+# shutil.copy("main.py", "main2.py") # It works for files only
+# shutil.copytree(".tutorial", "mytutorial") # Used for directories
+# shutil.move(".tutorial/file.file", "file.file") # Used for directories
+# shutil.rmtree("mytutorial")
+
+# shutil.rmtree(".tutorial")
+
+# os.remove("file.file")
+
