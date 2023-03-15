d57d62dbf13b73b83cb34b249c87dbd5869d24a1
Aniket <aniket.saini.ug20@nsut.ac.in>
2023-03-15 09:34:47 +0530

    95th Day Commit

---
+++ Day95RegularExpression/main.py
@@ -0,0 +1,43 @@
+import re
+
+# Metacharacters in regular expressions
+
+# []  Represent a character class
+# ^   Matches the beginning
+# $   Matches the end
+# .   Matches any character except newline
+# ?   Matches zero or one occurrence.
+# |   Means OR (Matches with any of the characters
+#     separated by it.
+# *   Any number of occurrences (including 0 occurrences)
+# +   One or more occurrences
+# {}  Indicate number of occurrences of a preceding RE 
+#     to match.
+# ()  Enclose a group of REs
+
+# https://www.ibm.com/docs/en/rational-clearquest/9.0.1?topic=tags-meta-characters-in-regular-expressions
+
+pattern = r"[A-Z]+yclone"
+text = '''Cyclone Dumazile was a strong tropical 
+cyclone in the South-West Indian Ocean that affected 
+Madagascar and Réunion in early March 2018. Dumazile 
+originated from a cyclone Dyclone low-pressure area 
+that formed near Agaléga on 27 February. It became 
+a tropical disturbance on 2 March, and was named the 
+next day after attaining tropical storm status. 
+Dumazile reached its peak intensity on 5 March, 
+with 10-minute sustained winds of 165 km/h (105 mph), 
+1-minute sustained winds of 205 km/h (125 mph), and 
+a central atmospheric pressure of 945 hPa (27.91 inHg). 
+As it tracked southeastwards, Dumazile weakened steadily 
+over the next couple of days due to wind shear, and 
+became a post-tropical cyclone on 7 March'''
+
+# match = re.search(pattern, text)
+
+matches = re.finditer(pattern, text)
+
+for match in matches:
+    print(match.span())
+    print(text[match.span()[0] : match.span()[1]])
+
